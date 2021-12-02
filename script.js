e22dd6efd216b02836feac4622b1acc5ded7d1d6
debrajsa <debrajsa@mindspring.com>
2021-12-02 05:27:38 -0500

    1 file modified - script.js to get yourChoice; ComputerChoice and Display Result

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
 let userChoice;
 let computerChoice;
+let result;
 
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
     generateComputerChoice();
+    //getResult();    
   })
 );
 
@@ -26,4 +28,29 @@ function generateComputerChoice() {
   }
   computerChoiceDisplay.innerHTML = computerChoice;
   // console.log(randomNumber);
+
+  function getResult() {
+    if (computerChoice === userChoice) {
+      result = 'its a draw!';
+    }
+    if (computerChoice === 'rock' && userChoice === 'paper') {
+      result = 'you win!';
+    }
+    if (computerChoice === 'rock' && userChoice === 'scissors') {
+      result = 'you lost!';
+    }
+    if (computerChoice === 'paper' && userChoice === 'scissors') {
+      result = 'you win!';
+    }
+    if (computerChoice === 'paper' && userChoice === 'rock') {
+      result = 'you lose!';
+    }
+    if (computerChoice === 'scissors' && userChoice === 'rock') {
+      result = 'you win!';
+    }
+    if (computerChoice === 'scissors' && userChoice === 'paper') {
+      result = 'you lose!';
+    }
+    resultDisplay.innerHTML = result;
+  }
 }
