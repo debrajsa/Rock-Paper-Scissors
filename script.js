5a37545689b700a16344f9adb16c417c534009e4
debrajsa <debrajsa@mindspring.com>
2021-12-01 19:14:59 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// let playerSelection = prompt('Type your choice of rock, paper or scissors.');
+
+let playerSelection = 'rock';
+let computerSelection = computerPlay();
+
+function computerPlay() {}
+
+function playGame(playerSelection, computerSelection) {}
+
+playGame(playerSelection, computerSelection);
